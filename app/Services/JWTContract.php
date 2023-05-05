09f3a737e620570cb0c8c420419a16dce63bdfae
Andre <andre.webderking@gmail.com>
2023-05-05 20:47:03 -0300

    add custom jwt service impl and bindings

---
+++ app/Services/JWTContract.php
@@ -0,0 +1,13 @@
+<?php
+
+
+namespace App\Services;
+
+
+interface JWTContract {
+    public function createToken(string $identifier);
+    public function getToken(string $uuid): string;
+    public function parseToken(string $jwt);
+    public function validateToken(string $jwt): bool;
+    public function getTokenClaims(string $jwt): array;
+}
