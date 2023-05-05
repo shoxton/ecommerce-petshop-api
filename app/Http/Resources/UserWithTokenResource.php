74dce066b679e5034cb86ef5ca1e464d0ce83cb5
Andre <andre.webderking@gmail.com>
2023-05-05 20:51:13 -0300

    add user with token resource

---
+++ app/Http/Resources/UserWithTokenResource.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class UserWithTokenResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return array_merge(parent::toArray($request), ['token' => $this->token()]);
+    }
+}
