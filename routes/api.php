fca6565f7d4a68102d56ffd7ea10c561d77e54ab
Andre <andre.webderking@gmail.com>
2023-05-05 20:51:54 -0300

    setup user crud and login routes

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\UserController;
+use App\Http\Middleware\JwtAuthMiddleware;
+use App\Services\JwtService;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,5 +19,20 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::prefix('v1')->group(function () {
-    Route::apiSingleton('user', UserController::class)->creatable();
+
+    Route::prefix('user')->name('user.')->group(function () {
+
+        Route::post('create', [UserController::class, 'store'])->name('store');
+        Route::get('/', [UserController::class, 'show'])->name('show')->middleware('jwt');
+        Route::put('edit', [UserController::class, 'update'])->name('update')->middleware('jwt');
+        Route::delete('/', [UserController::class, 'destroy'])->name('destroy')->middleware('jwt');
+
+        Route::post('login', [UserController::class, 'login'])->name('login');
+        Route::get('logout', [])->name('logout');
+
+        Route::post('forgot-password', [])->name('forgot-password');
+        Route::post('reset-password-token', [])->name('reset-password-token');
+
+        Route::get('orders', [])->name('orders')->middleware('jwt');
+    });
 });
