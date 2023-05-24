40950493972ef2d9e59abebc3f886d5396db6542
André Pereira <andrepereirapink@gmail.com>
2023-05-24 18:27:04 -0300

    add support for product uuid

---
+++ tests/Unit/ProductTest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Tests\Unit;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class ProductTest extends TestCase
+{
+
+    use RefreshDatabase;
+
+    public function test_uuid_is_automatically_filled(): void
+    {
+
+        $product = \App\Models\Product::factory()->create();
+
+        $this->assertNotNull($product->uuid);
+
+    }
+}
