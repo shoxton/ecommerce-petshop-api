b6f00be12bec89085e94fbd0a0503a5c72c0ee54
André Pereira <andrepereirapink@gmail.com>
2023-05-23 21:32:18 -0300

    remove unused old jwt config

--- config/services.php
+++ config/services.php
@@ -29,10 +29,5 @@ return [
         'key' => env('AWS_ACCESS_KEY_ID'),
         'secret' => env('AWS_SECRET_ACCESS_KEY'),
         'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
-    ],
-
-    'jwt' => [
-        'private' => env('JWT_KEY_PRIVATE_PATH', ''),
-        'public' => env('JWT_KEY_PUBLIC_PATH', '')
     ]
 ];
