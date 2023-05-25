1449c0f87c1d5dfdcb5d62bff942d2685bc7be0c
André Pereira <andrepereirapink@gmail.com>
2023-05-25 17:21:28 -0300

    overwrite default route key name on uuid traid

--- app/Traits/HasUuid.php
+++ app/Traits/HasUuid.php
@@ -4,10 +4,21 @@ namespace App\Traits;
 
 trait HasUuid
 {
+    /**
+     * Bootstrap trait to set uuid on creating event.
+     */
     protected static function bootHasUuid(): void
     {
         static::creating(function ($model) {
             $model->uuid = \Illuminate\Support\Str::uuid();
         });
+    }
+
+    /**
+     * Get the route key for the model.
+     */
+    public function getRouteKeyName(): string
+    {
+        return 'uuid';
     }
 }
