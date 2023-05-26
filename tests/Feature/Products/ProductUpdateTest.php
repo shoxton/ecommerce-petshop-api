4b3294355a2c596ef7def82c040496b7b0324be7
shoxton <shoxton@users.noreply.github.com>
2023-05-26 22:16:02 +0000

    PHP Linting (Pint)

--- tests/Feature/Products/ProductUpdateTest.php
+++ tests/Feature/Products/ProductUpdateTest.php
@@ -2,8 +2,6 @@
 
 namespace Tests\Feature\Products;
 
-use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class ProductUpdateTest extends TestCase
